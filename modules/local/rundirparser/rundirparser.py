5546c1069db9cf295166b34201a1cceb84e92647
Alfred Kedhammar <alfred.kedhammar@scilifelab.se>
2025-03-25 11:11:56 +0000

    generate _mqc.txt dummy file with table from rundir keys

--- modules/local/rundirparser/rundirparser.py
+++ modules/local/rundirparser/rundirparser.py
@@ -5,22 +5,58 @@ import yaml
 
 def parse_rundir(rundir):
     # Dummy implementation, replace with actual logic
-    return_dict = {
-        "rundir": rundir,
-        "samples": [
-            {"sample_id": "SAMPLE_1", "metric_1": 10, "metric_2": 20},
-            {"sample_id": "SAMPLE_2", "metric_1": 15, "metric_2": 25},
-            {"sample_id": "SAMPLE_3", "metric_1": 20, "metric_2": 30},
-        ],
-    }
-    return return_dict
+
+    yml_contents = """# plot_type: 'table'
+# section_name: 'rundir stats'
+# description: 'dummy rundir stats'
+# pconfig:
+#     namespace: 'Cust Data'
+# headers:
+#     col1:
+#         title: '#Seqs'
+#         description: 'Number of sequences'
+#         format: '{:,.0f}'
+#     col2:
+#         title: 'Total bp'
+#         description: 'Total size of the dataset'
+#     col3:
+#         title: 'Avg'
+#         description: 'Average sequence length'
+#     col4:
+#         title: 'N50'
+#         description: '50% of the sequences are longer than this size'
+#     col5:
+#         title: 'N75'
+#         description: '75% of the sequences are longer than this size'
+#     col6:
+#         title: 'N90'
+#         description: '90% of the sequences are longer than this size'
+#     col7:
+#         title: 'Min'
+#         description: 'Length of the shortest sequence'
+#     col8:
+#         title: 'Max'
+#         description: 'Length of the longest sequence'
+#     col9:
+#         title: 'auN'
+#         description: 'Area under the Nx curve'
+#     col10:
+#         title: 'GC'
+#         description: 'Relative GC content (excluding Ns)'
+"""
+    tsv_contents = f"""Sample	col1	col2	col3	col4	col5	col6	col7	col8	col9	col10
+{rundir}	10	147806	14780.6000000	22507	16573	15322	22801.9181765	344	33340	NaN
+"""
+
+    contents = yml_contents + tsv_contents
+
+    with open(f"{rundir}_mqc.txt", "w") as f:
+        f.write(contents)
 
 
 def main():
     rundir = sys.argv[1]
-    metadata = parse_rundir(rundir)
-    with open(f"{rundir}_mqc.yml", "w") as outfile:
-        yaml.dump(metadata, outfile, default_flow_style=False)
+    parse_rundir(rundir)
 
 
 if __name__ == "__main__":
